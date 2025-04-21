2f52a4a0a2fc097cdc8c709bc680bba88ca143cb
SAI ABHISHEK KETEPALLY <ketepask@mail.uc.edu>
2025-04-21 04:19:02 -0400

    set up prisma and implement register route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser"
 
 dotenv.config();
 
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.get("/", (req, res) => {
+    res.send("Hello guys welcome to leetlab!🔥")
+})
+
+app.use("/api/v1/auth", authRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("server is running on",process.env.PORT)
